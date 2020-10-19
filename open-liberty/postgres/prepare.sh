3774790b11be72f01fb0a3ae64814b73458f252b
Sebastian Daschner <git@sebastian-daschner.de>
2020-10-19 14:32:38 +0200

    updated Maven central URL

--- open-liberty/postgres/prepare.sh
+++ open-liberty/postgres/prepare.sh
@@ -4,4 +4,4 @@ set -eu
 
 # avoid storing the jar files in VCS
 
-wget http://central.maven.org/maven2/org/postgresql/postgresql/9.4.1212/postgresql-9.4.1212.jar -O postgresql-9.4.1212.jar
+wget https://repo.maven.apache.org/maven2/org/postgresql/postgresql/9.4.1212/postgresql-9.4.1212.jar -O postgresql-9.4.1212.jar
